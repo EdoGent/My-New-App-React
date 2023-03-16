8be638517160024681208142a284a84e6d2e53ac
Edoardo Gentile <edogen22122003@gmail.com>
2023-03-16 18:37:15 +0100

    Components-03 exercise

---
+++ src/Components/Components-03.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hello from './Components-02';
+
+class App extends React.Component {
+    render () {
+        return (
+        <div>
+            <Hello />
+        </div>
+        )
+    }
+}
+
+export default App;
